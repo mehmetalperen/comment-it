ff16ab1c6f91afd2b4d549ffbd5e1c9cf56b8ffb
mehmetalperen <mhmtalperennadi@gmail.com>
2022-03-29 17:07:37 -0700

    create AddReview.jsx and relocate App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Login from "../pages/Login";
-import Signup from "../pages/Signup";
-import Greeting from "../pages/Greeting";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Greeting from "./pages/Greeting";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "../contexts/AuthContext";
-import Comments from "../pages/Comments";
+import { AuthProvider } from "./contexts/AuthContext";
+import Comments from "./pages/Comments";
 
 function App() {
   const [isNewUser, setIsNewUser] = useState(true);
