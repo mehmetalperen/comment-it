e5bd40cfae580dddc955c62b607902bddd7e3fb9
mehmetalperen <mhmtalperennadi@gmail.com>
2022-03-31 16:34:21 -0700

    adding comment/review front-end is ready

--- src/components/AddReview.jsx
+++ src/components/AddReview.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { Collapse } from "react-bootstrap";
 
 export default function AddReview() {
+  const [showComment, setShowComment] = useState(false);
+  const [starReview, setStarReview] = useState(0);
+  const [comment, setComment] = useState("");
+
+  const toggleHideComment = (bool = true) => {
+    setShowComment(bool);
+  };
+
+  const handleStarReview = (e) => {
+    setStarReview(e.target.value);
+  };
+
+  const resetReview = () => {
+    setComment("");
+    setStarReview(0);
+    toggleHideComment(false);
+  };
+
+  const dummySubmit = () => {
+    console.log(`star: ${starReview} \n comment: ${comment}`);
+    resetReview();
+  };
   return (
     <>
-      <div className="row d-flex border">
-        <div className="col-4 d-flex justify-content-between border border-danger">
+      <hr />
+      <div className="row d-flex">
+        <div className="col-4 d-flex justify-content-between">
           <div className=" mb-3">
             <div className="row no-gutters">
               <div className="col-md-4">
@@ -23,18 +47,26 @@ export default function AddReview() {
             </div>
           </div>
         </div>
-        <div className="col-8 d-flex border border-success justify-content-center">
-          <div class="container d-flex justify-content-center ">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="start-card">
-                  <div class="start-card-body text-center">
-                    <fieldset class="rating">
-                      {" "}
-                      <input type="radio" id="star5" name="rating" value="5" />
+        <div className="col-8">
+          <div className="container d-flex justify-content-center ">
+            <div className="row mt-2 d-flex justify-content-center">
+              <div className="col-md-6">
+                <div className="start-card">
+                  <div className="start-card-body text-center">
+                    <fieldset className="rating">
+                      <input
+                        type="radio"
+                        id="star5"
+                        name="rating"
+                        value="5"
+                        onClick={(e) => {
+                          toggleHideComment(true);
+                          handleStarReview(e);
+                        }}
+                      />
                       <label
-                        class="full"
-                        for="star5"
+                        className="full"
+                        htmlFor="star5"
                         title="Awesome - 5 stars"
                       ></label>{" "}
                       <input
@@ -42,16 +74,29 @@ export default function AddReview() {
                         id="star4half"
                         name="rating"
                         value="4.5"
+                        onClick={(e) => {
+                          toggleHideComment(true);
+                          handleStarReview(e);
+                        }}
                       />
                       <label
-                        class="half"
-                        for="star4half"
+                        className="half"
+                        htmlFor="star4half"
                         title="Pretty good - 4.5 stars"
                       ></label>{" "}
-                      <input type="radio" id="star4" name="rating" value="4" />
+                      <input
+                        type="radio"
+                        id="star4"
+                        name="rating"
+                        value="4"
+                        onClick={(e) => {
+                          toggleHideComment(true);
+                          handleStarReview(e);
+                        }}
+                      />
                       <label
-                        class="full"
-                        for="star4"
+                        className="full"
+                        htmlFor="star4"
                         title="Pretty good - 4 stars"
                       ></label>{" "}
                       <input
@@ -59,16 +104,29 @@ export default function AddReview() {
                         id="star3half"
                         name="rating"
                         value="3.5"
+                        onClick={(e) => {
+                          toggleHideComment(true);
+                          handleStarReview(e);
+                        }}
                       />
                       <label
-                        class="half"
-                        for="star3half"
+                        className="half"
+                        htmlFor="star3half"
                         title="Meh - 3.5 stars"
                       ></label>{" "}
-                      <input type="radio" id="star3" name="rating" value="3" />
+                      <input
+                        type="radio"
+                        id="star3"
+                        name="rating"
+                        value="3"
+                        onClick={(e) => {
+                          toggleHideComment(true);
+                          handleStarReview(e);
+                        }}
+                      />
                       <label
-                        class="full"
-                        for="star3"
+                        className="full"
+                        htmlFor="star3"
                         title="Meh - 3 stars"
                       ></label>{" "}
                       <input
@@ -76,16 +134,29 @@ export default function AddReview() {
                         id="star2half"
                         name="rating"
                         value="2.5"
+                        onClick={(e) => {
+                          toggleHideComment(true);
+                          handleStarReview(e);
+                        }}
                       />
                       <label
-                        class="half"
-                        for="star2half"
+                        className="half"
+                        htmlFor="star2half"
                         title="Kinda bad - 2.5 stars"
                       ></label>{" "}
-                      <input type="radio" id="star2" name="rating" value="2" />
+                      <input
+                        type="radio"
+                        id="star2"
+                        name="rating"
+                        value="2"
+                        onClick={(e) => {
+                          toggleHideComment(true);
+                          handleStarReview(e);
+                        }}
+                      />
                       <label
-                        class="full"
-                        for="star2"
+                        className="full"
+                        htmlFor="star2"
                         title="Kinda bad - 2 stars"
                       ></label>{" "}
                       <input
@@ -93,16 +164,29 @@ export default function AddReview() {
                         id="star1half"
                         name="rating"
                         value="1.5"
+                        onClick={(e) => {
+                          toggleHideComment(true);
+                          handleStarReview(e);
+                        }}
                       />
                       <label
-                        class="half"
-                        for="star1half"
+                        className="half"
+                        htmlFor="star1half"
                         title="Meh - 1.5 stars"
                       ></label>{" "}
-                      <input type="radio" id="star1" name="rating" value="1" />
+                      <input
+                        type="radio"
+                        id="star1"
+                        name="rating"
+                        value="1"
+                        onClick={(e) => {
+                          toggleHideComment(true);
+                          handleStarReview(e);
+                        }}
+                      />
                       <label
-                        class="full"
-                        for="star1"
+                        className="full"
+                        htmlFor="star1"
                         title="Sucks big time - 1 star"
                       ></label>{" "}
                       <input
@@ -110,17 +194,25 @@ export default function AddReview() {
                         id="starhalf"
                         name="rating"
                         value="0.5"
+                        onClick={(e) => {
+                          toggleHideComment(true);
+                          handleStarReview(e);
+                        }}
                       />
                       <label
-                        class="half"
-                        for="starhalf"
+                        className="half"
+                        htmlFor="starhalf"
                         title="Sucks big time - 0.5 stars"
                       ></label>{" "}
                       <input
                         type="radio"
-                        class="reset-option"
+                        className="reset-option"
                         name="rating"
                         value="reset"
+                        onClick={(e) => {
+                          toggleHideComment(true);
+                          handleStarReview(e);
+                        }}
                       />{" "}
                     </fieldset>
                   </div>
@@ -128,6 +220,40 @@ export default function AddReview() {
               </div>
             </div>
           </div>
+        </div>
+        <div className="col-12">
+          <Collapse in={showComment}>
+            <div id="example-collapse-text" className="">
+              <div className="form-group mt-3 mb-2">
+                <textarea
+                  className="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="2"
+                  placeholder="Add your comment here..."
+                  value={comment}
+                  onChange={(e) => {
+                    setComment(e.target.value);
+                  }}
+                ></textarea>
+              </div>
+              <div className="d-flex justify-content-end">
+                <div className="w-50 align-middle d-flex justify-content-between">
+                  <button
+                    className="btn btn-primary d-grid  mx-2 w-50"
+                    onClick={resetReview}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="d-grid  mx-2 w-50 btn btn-secondary"
+                    onClick={dummySubmit}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Collapse>
         </div>
       </div>
     </>
