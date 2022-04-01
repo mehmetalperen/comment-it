90fb20842092159091af4494ce5a3c7caa9d8124
mehmetalperen <mhmtalperennadi@gmail.com>
2022-03-31 17:18:51 -0700

    add start icons

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import UserProfileCard from "./UserProfileCard";
-
+import StarIcon from "@material-ui/icons/Star";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import StarHalfIcon from "@material-ui/icons/StarHalf";
 export default function CommentCard() {
   return (
     <>
-      <div className="row d-flex">
+      <div className="row d-flex mb-5">
         <hr />
-        <div className="col-4">
+        <div className="col-4 ">
           <UserProfileCard />
+          <div className="start-review-wrapper m-2">
+            <p>
+              <StarIcon className="star-icon" />
+              <StarIcon className="star-icon" />
+              <StarIcon className="star-icon" />
+              <StarHalfIcon className="star-icon" />
+              <StarBorderIcon className="star-icon" />
+            </p>
+          </div>
+        </div>
+        <div className="comment-content">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+          nisi repellat similique blanditiis, hic debitis consectetur,
+          praesentium eius, minus distinctio harum officia corrupti obcaecati et
+          laudantium veniam ea minima.
         </div>
       </div>
     </>
