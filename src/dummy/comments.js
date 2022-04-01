403409824a408c77c73fac88e182b9b9fd05d3ba
mehmetalperen <mhmtalperennadi@gmail.com>
2022-03-31 17:26:18 -0700

    add dummy data

---
+++ src/dummy/comments.js
@@ -0,0 +1,148 @@
+const comments = [
+  {
+    fName: "Mehmet",
+    lName: "Nadi",
+    starReview: 4,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Alperen",
+    lName: "Nadi",
+    starReview: 1.5,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Nadi",
+    lName: "Nadi",
+    starReview: 4.5,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Nadi",
+    lName: "Mehmet",
+    starReview: 5,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Nadi",
+    lName: "Alperen",
+    starReview: 0.5,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Yusuf",
+    lName: "Nadi",
+    starReview: 2.5,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Nadi",
+    lName: "Yusuf",
+    starReview: 3.4,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Cihan",
+    lName: "Nadi",
+    starReview: 4.5,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Cemile",
+    lName: "Nadi",
+    starReview: 4,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Huma",
+    lName: "Nadi",
+    starReview: 2,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Cengiz",
+    lName: "Nadi",
+    starReview: 1,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Erdal",
+    lName: "Nadi",
+    starReview: 1.5,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Hilal",
+    lName: "Nadi",
+    starReview: 4,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Hatice",
+    lName: "Nadi",
+    starReview: 4,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Omer",
+    lName: "Nadi",
+    starReview: 2.5,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+  {
+    fName: "Safiye",
+    lName: "Nadi",
+    starReview: 3,
+    comment: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ut
+        nisi repellat similique blanditiis, hic debitis consectetur,
+        praesentium eius, minus distinctio harum officia corrupti obcaecati et
+        laudantium veniam ea minima.`,
+  },
+];
+
+export default comments;
