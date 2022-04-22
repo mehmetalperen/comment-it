45905e8d6cbda58429a5f98e073008d97cfe5fe6
mehmetalperen <mhmtalperennadi@gmail.com>
2022-04-21 17:05:36 -0700

    data coming from firebase not from dummy. able to create comments and reviews and push it to firebase realtime database

--- src/App.js
+++ src/App.js
@@ -8,73 +8,64 @@ import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import Comments from "./pages/Comments";
 import firebase from "./firebase";
-import { faL } from "@fortawesome/free-solid-svg-icons";
-/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-ABLE TO ADD REVIEW TO THE FIREBASE DB BUT THE FIRST COMMENT IS SUBMITED TWICE. 
-STILL NEED TO RENDER REAL DATA COMING FROM FIREBASE INSTEAD OF DUMMY. TOTAL REVIEW AND COMMENT COUNT DOESN'T WORK AS EXPECTED.
-THE APP IS VERY INEFFICENT, RUNTIME VISE.
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+
 function App() {
   const [url, setUrl] = useState("");
   const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState();
+  const [database, setDatabase] = useState();
   const [dataID, setDataID] = useState("");
   const [reviews, setReviews] = useState();
+  const [isRetriveDataRun, setRetriveDataRun] = useState(false);
 
-  /**
-   * Get current URL
-   */
   useEffect(() => {
     const queryInfo = { active: true, lastFocusedWindow: true };
 
     chrome.tabs &&
       chrome.tabs.query(queryInfo, (tabs) => {
         const url = tabs[0].url;
-        setUrl("http://localhost:3000/"); //change this to setUrl(url)
+        setUrl(url);
       });
-
-    setUrl("http://localhost:3000/"); //DELETE THIS. chrome extention version won't have this
   }, []);
 
-  const createNewSite = () => {
+  const retriveData = () => {
+    const websiteRef = firebase.database().ref("Websites");
+
+    websiteRef.on("value", (snapshot) => {
+      for (let key in snapshot.val()) {
+        //big-o of n. need to optimize this.
+        if (snapshot.val()[key].websiteURL === url) {
+          setDataID(key);
+          setIsLoading(false);
+
+          break;
+        }
+      }
+      setRetriveDataRun(true);
+    });
+  };
+
+  const pushWebsiteToDB = () => {
     const websiteRef = firebase.database().ref("Websites");
     const newSite = websiteRef.push();
     newSite.set({
       websiteURL: url,
       reviews: [""],
     });
-
     setDataID(newSite.key);
-  };
-
-  const retriveData = () => {
-    const websiteRef = firebase.database().ref("Websites");
-    websiteRef.on("value", (snapshot) => {
-      for (let key in snapshot.val()) {
-        //big-o of n. need to optimize this.
-        if (snapshot.val()[key].websiteURL === url) {
-          setDataID(key);
-        }
-      }
-    });
-
     setIsLoading(false);
   };
 
   useEffect(() => {
-    if (url !== "") {
-      const websitesRef = firebase.database().ref("Websites");
-      websitesRef.on("value", (snapshot) => {
-        if (!snapshot.val()) {
-          createNewSite();
-        }
-      });
+    if (url) {
       retriveData();
     }
   }, [url]);
 
+  useEffect(() => {
+    if (isLoading && isRetriveDataRun) {
+      pushWebsiteToDB();
+    }
+  }, [isRetriveDataRun]);
   return (
     <Router>
       <AuthProvider>
